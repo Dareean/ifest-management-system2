b186adc388c255191c7f64623b49396bd8887f89
Dareean <dmardin@gmail.com>
2026-07-20 15:28:57 +0800

    build: untrack build and temporary folders .open-next and .wrangler

--- .open-next/server-functions/default/.next/server/app/api/cron/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/cron/route.js")
-R.c("server/chunks/[externals]_next_dist_1ce_grm._.js")
-R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_1_ab4na.js")
-R.c("server/chunks/[root-of-the-server]__0xuaoik._.js")
-R.c("server/chunks/_next-internal_server_app_api_cron_route_actions_0hfzp9k.js")
-R.m(31944)
-module.exports=R.m(31944).exports
